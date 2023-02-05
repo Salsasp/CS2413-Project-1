b9cbc8634804d6c47dad976627ad2cd91f8405e2
Salsasp <dakotagee99@gmail.com>
2023-02-05 16:25:21 -0600

    complete

--- Project1.cpp
+++ Project1.cpp
@@ -5,6 +5,7 @@
 #include <time.h> // for experiments
 using namespace std;
 
+//Declaration of all global variables: Arrays and sizes of the arrays
 int* A;
 int ASize;
 int* B;
@@ -18,6 +19,7 @@ int rsize;
 int* insertionOperations;
 int isize;
 
+//linear search function for unsorted arrays
 int linearSearch(int* array, int target, int size)
 {
     for(int i = 0; i < size; i++)
@@ -30,6 +32,7 @@ int linearSearch(int* array, int target, int size)
     return -1;
 }
 
+//recursive binarySearch function for sorted arrays
 int binarySearch(int* array, int upper, int lower, int target)
 {
     //base case
@@ -47,13 +50,16 @@ int binarySearch(int* array, int upper, int lower, int target)
     //if the target is in the lower half of the array, recur function
     else if(target < array[middle])
         return binarySearch(array, middle-1, lower, target);
+    return -1;
 }
 
+//function that finds two integer values in a given array that add up to the integer argument 'target'
 int* sumPairs(int*array, int target, int size)
 {
-    static int pair[2];
+    static int pair[2]; //int array to store pair values
     int val1;
     int val2;
+    //nested for loop that checks each element's sum with other elements
     for(int i = 0; i < size; i++)
     {
         val1 = array[i];
@@ -71,6 +77,7 @@ int* sumPairs(int*array, int target, int size)
     return NULL;
 }
 
+//function to remove a given target from a given array and shift the remaining elements
 int removeElement(int* array, int target, int size, char arrName)
 {
     
@@ -91,6 +98,8 @@ int removeElement(int* array, int target, int size, char arrName)
     return -1;
 }
 
+//function that inserts an element into an array and shifts the remaining elements
+//if the array is unsorted, the elements are added to the end
 void insertElement(int toInsert, int size, int *array)
 {
     //check whether array being operated on is A or B
@@ -110,6 +119,7 @@ void insertElement(int toInsert, int size, int *array)
     BSize++;
 }
 
+//function that utilizes the bubblesort algorithm to sort array data
 int* sort(int* toSort, int maxValues)
 {
     int* B = new int[maxValues]; //create dynamic array B
@@ -142,8 +152,8 @@ int main()
     int currentValues; // as we add, the number of values in the arrays
     int tempVal; // variable used to read the number 
     cin >> maxValues; // read the maximum number of values from the redirected input file
-    ASize = maxValues;
-    BSize = maxValues;
+    ASize = maxValues; //set global variable ASize equal to current maxValues
+    BSize = maxValues; //set global variable BSize equal to current maxValues
     cout << "Length of the array: " << maxValues << endl;
 
     A = new int[maxValues];//dynamic array containing unsorted values
@@ -160,6 +170,7 @@ int main()
         cout << A[i] << " ";
     }
     cout << '\n';
+
     // sort the array A and store it in B
     B = sort(A, maxValues);
     cout<<"Array B: ";
@@ -178,7 +189,7 @@ int main()
     {
     cin >> operation;
     cin >> size;
-       switch (operation)
+       switch (operation) //checks which operation must be read in (in the case of randomized operation order in input data)
         {
         case 'F':
             findQueries = new int[size];
@@ -208,9 +219,10 @@ int main()
             cin >> currentOp[i];
         }
     }
-/**************************************************************************************************************************************/
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //output section
 //output for search queries
+
     cout<<"Find: "<<'\n';
     for(int i = 0; i < fsize; i++)
     {
@@ -218,6 +230,7 @@ int main()
             cout<<"Element "<< findQueries[i] << "  found at " << binarySearch(B, maxValues, 0, findQueries[i]) << " in B" << '\n';
         else cout<<"Element "<< findQueries[i] << "  not found in B" << '\n';
     }
+
     cout<<'\n';
     for(int i = 0; i < fsize; i++)
     {
@@ -236,6 +249,7 @@ int main()
         if(pair != NULL)
             cout << " = " << pair[0]<<" + "<<pair[1]<<'\n';
     }
+
     cout << '\n';
     cout<<"A:"<<'\n';
     for(int i = 0; i < asize; i++)
@@ -256,6 +270,7 @@ int main()
             cout<<"Removing " << removeOperations[i] << " at " << index << " in " << "B" << '\n';
         else cout << "Element " << removeOperations[i] << " not found in " << "B" << '\n';
     }
+
     cout << '\n';
     for(int i = 0; i < rsize; i++)
     {
@@ -264,6 +279,7 @@ int main()
             cout<<"Removing " << removeOperations[i] << " at " << index << " in " << "A" << '\n';
         else cout << "Element " << removeOperations[i] << " not found in " << "A" << '\n';
     }
+
     cout << '\n';
     cout << "After removal Array A: ";
     for(int i = 0; i < ASize; i++)
@@ -276,6 +292,7 @@ int main()
         cout<<B[i]<<" ";
     }
     cout<<'\n'<<'\n'<<"Insert:"<<'\n';
+
     //output for insertion operations
     for(int i = 0; i < isize; i++)
     {
@@ -283,6 +300,7 @@ int main()
         cout<<"Inserting " << insertionOperations[i] << " in B";
         cout<<'\n';
     }
+
     cout<<'\n';
     for(int i = 0; i < isize; i++)
     {
@@ -290,6 +308,7 @@ int main()
         cout<<"Inserting " << insertionOperations[i] << " in A";
         cout<<'\n';
     }
+
     cout << '\n' << "After insertions Array A: ";
     for(int i = 0; i < ASize; i++)
     {
@@ -300,6 +319,7 @@ int main()
     {
         cout<<B[i] << " ";
     }
+
 
 
     //delete all arrays to clear memory
